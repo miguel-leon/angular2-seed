bc2a1c36eb9c7f69aa911dbf43b538e16a7e52ec
Miguel <miguel.e.leon.f@gmail.com>
2016-09-08 16:37:02 -0300

    copy firts files and merge package.json

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	moduleId:    module.id,
+	selector:    'oli-app',
+	templateUrl: 'app.component.html'
+})
+export class AppComponent implements OnInit {
+
+	constructor() {
+	}
+
+	ngOnInit() {
+	}
+
+}
